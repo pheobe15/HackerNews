6abd84e39ffbc285de0b15d036235d86949654f4
pheobe15 <43532297+pheobe15@users.noreply.github.com>
2023-03-01 12:03:22 +0530

    Create Story.java

---
+++ src/Story.java
@@ -0,0 +1,40 @@
+public class Story {
+  
+  private String title;
+  private String url;
+  private int score;
+  private long time;
+  private String by;
+  
+  // Getters and setters
+  public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public int getScore() {
+			return score;
+		}
+		public void setScore(int score) {
+			this.score = score;
+		}
+		public long getTime() {
+			return time;
+		}
+		public void setTime(long time) {
+			this.time = time;
+		}
+		public String getBy() {
+			return by;
+		}
+		public void setBy(String by) {
+			this.by = by;
+		}
+}
