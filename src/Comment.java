7c1bc3bb7757ec6b9333ce5ecd339549fbf72e9f
pheobe15 <43532297+pheobe15@users.noreply.github.com>
2023-03-01 12:05:21 +0530

    Create Comment.java

---
+++ src/Comment.java
@@ -0,0 +1,19 @@
+public class Comment {
+  
+  private String text;
+  private String by;
+  
+  // Getters and setters
+  public String getText() {
+			return text;
+		}
+		public void setText(String text) {
+			this.text = text;
+		}
+		public String getBy() {
+			return by;
+		}
+		public void setBy(String by) {
+			this.by = by;
+		}
+}
