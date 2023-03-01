45e95d61287dcebd4cd2846482b66864d96448d2
pheobe15 <43532297+pheobe15@users.noreply.github.com>
2023-03-01 16:56:43 +0530

    Update Application.java

--- src/main/java/com.example/Application.java
+++ src/main/java/com.example/Application.java
@@ -1,1 +1,14 @@
+package com.example.hackernewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
 
